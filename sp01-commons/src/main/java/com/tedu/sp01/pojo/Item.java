1cba14709803d797279669d0cd745ef8914b81ba
LIuwei0422 <952017437@qq.com>
2019-06-28 11:08:13 +0800

    454

---
+++ sp01-commons/src/main/java/com/tedu/sp01/pojo/Item.java
@@ -0,0 +1,18 @@
+package com.tedu.sp01.pojo;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Item {
+	//±àºÅ
+	private Integer id;
+	//³åÍ»
+	//ÐÕÃû,tan
+	private String name;
+	//ÊýÁ¿
+	private Integer number;
+}
